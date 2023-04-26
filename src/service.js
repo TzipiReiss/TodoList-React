29d61898a6e24fc65093b0d2f3a686ece1d567f4
TzipiReiss <tz0583204266@gmail.com>
2023-04-26 14:53:37 +0300

    first commit

---
+++ src/service.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://localhost:7094/items';
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+export default {
+  getTasks: async () => {
+    const result = await axios.get();
+    return result.data;
+  },
+
+  addTask: async (name) => {
+    const result = await axios.post('',{ Name: name, IsComplete: false })
+    return result.data;
+  },
+
+  setCompleted: async (id, isComplete) => {
+    const result = await axios.put(`/${id}?IsComplete=${isComplete}`)
+    return result.data;
+  },
+
+  deleteTask: async (id) => {
+    const result = await axios.delete(`/${id}`);
+    return result.data;
+  }
+};
