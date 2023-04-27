4632d487a6ecd07b29c04f56ba2ff9f637de6000
TzipiReiss <tz0583204266@gmail.com>
2023-04-27 14:31:01 +0300

    Add .env

---
+++ build/precache-manifest.dfe2588f5bfa86dd24f75ce144e00d6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1363cc109644af80f1d2fc74e341c2f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3152933934f9cff92e3a",
+    "url": "/static/js/2.dbfef003.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/static/js/2.dbfef003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ccf002545c1d00cc2c1",
+    "url": "/static/js/main.a8965a8e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.a8965a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed24628c41b89e511d36",
+    "url": "/static/js/runtime-main.76e1faf0.js"
+  }
+]);
